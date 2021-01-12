f6f2b4f992173223f129a721146947319eddf847
Mark Lin <marklin2289@gmail.com>
2021-01-12 14:35:21 -0800

    javascript assignment and basic

---
+++ assignment/basics-assignment-1-start/assignment.js
@@ -0,0 +1,8 @@
+const userInput = 5;
+// let result = 0;
+let result;
+result = userInput + 18;
+result = result - 1;
+result = result * 2;
+result = result / 2;
+alert(`result :${result} ,  userInput: ${userInput}`);
---
+++ basics-01-starting-project/assets/scripts/app.js
@@ -0,0 +1,6 @@
+// alert("This works!");
+let currentResult = 0;
+
+currentResult = currentResult + 10;
+
+outputResult(currentResult, "");
