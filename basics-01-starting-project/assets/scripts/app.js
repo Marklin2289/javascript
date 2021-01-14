95203fede8bdb3c122828cb2dcf435395be65b8c
Mark Lin <marklin2289@gmail.com>
2021-01-13 17:53:27 -0800

    calculator note

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,6 +1,56 @@
-// alert("This works!");
-let currentResult = 0;
+const defaultResult = 0;
+let currentResult = defaultResult;
 
-currentResult = currentResult + 10;
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
 
-outputResult(currentResult, "");
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription);
+}
+
+function add() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  // const calcDescription = `${currentResult} + ${enteredNumber}`;
+  //   currentResult = currentResult + enteredNumber;
+  currentResult += +enteredNumber;
+  createAndWriteOutput("+", initialResult, enteredNumber);
+  // outputResult(currentResult, calcDescription);
+}
+
+function subtract() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  // const calcDescription = `${currentResult} - ${enteredNumber}`;
+  //   currentResult = currentResult - enteredNumber;
+  currentResult -= +enteredNumber;
+  createAndWriteOutput("-", initialResult, enteredNumber);
+  // outputResult(currentResult, calcDescription);
+}
+
+function multiple() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  // const calcDescription = `${currentResult} * ${enteredNumber}`;
+  //   currentResult = currentResult * enteredNumber;
+  currentResult *= +enteredNumber;
+  createAndWriteOutput("*", initialResult, enteredNumber);
+  // outputResult(currentResult, calcDescription);
+}
+
+function divide() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  // const calcDescription = `${currentResult} / ${enteredNumber}`;
+  //   currentResult = currentResult / enteredNumber;
+  currentResult /= +enteredNumber;
+  createAndWriteOutput("/", initialResult, enteredNumber);
+  // outputResult(currentResult, calcDescription);
+}
+
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiple);
+divideBtn.addEventListener("click", divide);
